51760cb6d3df592009b8dc53a763920905482f98
krishnakumardev363 <krishnakumardev363>
2026-07-07 22:31:46 +0530

    Fixed

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "https://quizdtech.netlify.app/login",
+    origin: process.env.CLIENT_URL || "https://quizdtech.netlify.app/",
     credentials: true,
   })
 );
